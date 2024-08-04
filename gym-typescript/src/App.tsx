5f060e8b9bbcc220efa5933fb54cccd629f48004
Rita <jwna867@aucklanduni.ac.nz>
2024-08-04 14:57:22 +1200

    merge

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,40 +1,38 @@
 import { useEffect, useState } from "react";
 import Navbar from "./scenes/navbar";
 import { SelectedPage } from "./shared/types";
-import Home from "./scenes/Home"
-import Benefits from "./scenes/Benefits"
-
-
+import Home from "./scenes/Home";
+import Benefits from "./scenes/Benefits";
 
 function App() {
-  const [selectedPage, setSelectedPage]= useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-  
-  useEffect(()=>{
-    const handleScroll =()=>{
-      if(window.scrollY ===0){
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
         setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
       }
-      if(window.scrollY !==0) setIsTopOfPage(false);
-    }
-    window.addEventListener("scroll",handleScroll);
-    return ()=>window.removeEventListener("scroll",handleScroll)
-  },[]);
-  
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className= "app bg-gray-20">
-      <Navbar 
-      isTopOfPage = {isTopOfPage}
-          selectedPage ={selectedPage}
-          setSelectedPage={setSelectedPage}
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
-    <Home setSelectedPage={setSelectedPage} />
-    <Benefits setSelectedPage={setSelectedPage} />
-    
-  
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
